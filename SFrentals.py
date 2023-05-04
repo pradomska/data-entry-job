79b79c36db69303318ba99cb66af679bfbc37ae6
Paulina Radomska <113101087+pradomska@users.noreply.github.com>
2023-05-04 14:29:48 +0200

    Add files via upload

---
+++ SFrentals.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from time import sleep
+
+CHROME_DRIVE_PATH = "C:/Development/chromedriver.exe"
+FORMS_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeNPWebA7VGTlo2tyeJog_Rl0G7y6nuShxil-WK3u5WbunV2g/viewform?usp=sf_link"
+
+header = { 'Accept-Language': "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        }
+
+response = requests.get("https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D", headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".property-card-data a")
+
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    # print(href)
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".search-page-list-container address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".search-page-list-container span")
+all_prices = [price.get_text().split("+")[0] for price in all_price_elements if "$" in price.text]
+
+driver = webdriver.Chrome(service=Service(CHROME_DRIVE_PATH), options=webdriver.ChromeOptions())
+
+for n in range(len(all_links)):
+    driver.get(FORMS_URL)
+
+    sleep(4)
+    address = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    address.send_keys(all_addresses[n])
+    sleep(2)
+    price = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price.send_keys(all_prices[n])
+    sleep(2)
+    link = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link.send_keys(all_links[n])
+    sleep(2)
+    submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+    submit_button.click()
+    sleep(2)
+
+sleep(3)
+driver.quit()
